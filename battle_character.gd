25f9a5e6564f3f6373fcfdb679e2119c90e69670
eRues <ellie.rue@stu.daviess.kyschools.us>
2025-12-08 13:19:40 -0600

    abstract classes done

---
+++ battle_character.gd
@@ -0,0 +1,7 @@
+@abstract
+class_name Battle_Character extends Character
+
+@abstract func attack()
+@abstract func defend()
+@abstract func get_dodge()
+@abstract func get_crit()
--- character.gd
+++ character.gd
@@ -1,8 +1,11 @@
+@abstract
 class_name Character extends Node2D
 
-var health = 0
-var strength = 0
-var speeds = 0
-var smarts = 0
-var spirit = 0
-var agitation = 0
+var health
+var strength
+var speeds
+var smarts
+var spirit
+var agitation
+var level
+var experience 
