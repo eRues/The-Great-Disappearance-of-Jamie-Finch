7c560e335b9f712b97eb8ec1899d6aa3af5d999a
eRues <ellie.rue@stu.daviess.kyschools.us>
2025-12-15 13:20:57 -0600

    post battle worked on

---
+++ info_label.gd
@@ -0,0 +1,22 @@
+extends RichTextLabel
+func _ready() -> void:
+	self.visible = true
+
+func _on_visibility_changed() -> void:
+	self.clear()
+	self.add_text("Enemy Defeated!")
+	self.newline()
+	self.add_text("Level: " + str(Global.battle_type.level))
+	self.newline()
+	self.add_text("Experience: " + str(Global.battle_type.experience) + " / " + str(Global.battle_type.exp_to_next_level))
+	self.newline()
+	self.add_text("Health = " + str(Global.battle_type.health))
+	self.newline()
+	self.add_text("Strength = " + str(Global.battle_type.strength))
+	self.newline()
+	self.add_text("Speed = " + str(Global.battle_type.speed))
+	self.newline()
+	self.add_text("Smarts = " + str(Global.battle_type.smarts))
+	self.newline()
+	self.add_text("Spirit = " + str(Global.battle_type.spirit))
+	
