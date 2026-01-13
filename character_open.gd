6e18712c90d2330a79ab1d8f6cb642336c0b19ff
eRues <ellie.rue@stu.daviess.kyschools.us>
2026-01-13 13:27:42 -0600

    inventory continuation

--- character_open.gd
+++ character_open.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var interact_ui = $InteractUI
+
 func _ready() -> void:
 	$ash_open.visible = false
 	$tia_open.visible = false
--- inventory_item.gd
+++ inventory_item.gd
@@ -9,6 +9,8 @@ var scene_path: String = "res://inventory_item.tscn"
 
 @onready var icon_sprite = $Sprite2D
 
+var player_in_range = false
+
 func _ready() -> void:
 	if not Engine.is_editor_hint():
 		icon_sprite.texture = item_texture
@@ -16,3 +18,32 @@ func _ready() -> void:
 func _process(_delta) -> void:
 	if Engine.is_editor_hint():
 		icon_sprite.texture = item_texture
+	
+	if(player_in_range and Input.is_action_pressed("space")):
+		item_pickup()
+
+func item_pickup():
+	var item = {
+		"quantity": 1,
+		"item_type": item_type,
+		"item_name": item_name,
+		"item_desc": item_desc,
+		"item_texture": item_texture,
+		"item_effects": item_effects,
+		"scene_path": scene_path
+	}
+	
+	if(InventoryAutoload.player_node):
+		InventoryAutoload.add_item(item)
+		self.queue_free()
+
+
+func _on_area_2d_body_entered(body) -> void:
+	if(body.is_in_group("player")):
+		player_in_range = true
+		body.ui_interact.visible = true
+
+func _on_area_2d_body_exited(body) -> void:
+	if(body.is_in_group("player")):
+		player_in_range = false
+		body.ui_interact.visible = false
