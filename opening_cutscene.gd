b9519ca508753bcd0ca11df1a3ea61a93b6232c9
eRues <ellie.rue@stu.daviess.kyschools.us>
2026-02-04 12:34:29 -0600

    version updated, we're chilling and ready to publish

--- opening_cutscene.gd
+++ opening_cutscene.gd
@@ -3,6 +3,11 @@ extends Node2D
 func _ready() -> void:
 	$text_box.start()
 
+func _input(event: InputEvent) -> void:
+	if(event.is_action_pressed("space")):
+		$Label.visible = false
+		$KeySpace.visible = false
+
 func _on_temp_battle_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://battle_scene.tscn")
 
