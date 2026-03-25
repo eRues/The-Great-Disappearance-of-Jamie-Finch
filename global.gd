ca882957da13b923437982d898fb6164ce4cf613
eRues <ellie.rue@stu.daviess.kyschools.us>
2026-03-25 13:18:21 -0500

    character select updated

--- global.gd
+++ global.gd
@@ -16,9 +16,6 @@ var battle_type = Battle_Ash.new()
 #determines if running from a battle is possible
 var dont_run = false
 
-#which enemy will be shown in the battle screen
-var current_enemy = ""
-
 #agitation of the phlegathon, added to the agitation of all non-boss enemies
 var base_agitation = 1.00
 
